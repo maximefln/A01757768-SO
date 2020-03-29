a0b58fc6f9b9c62309bdb03b75c9cff335767249
maximefln <maxime.fillion@hei.yncrea.fr>
2020-03-29 13:58:43 +0000

    practica 4

--- practica4/testfork.c
+++ practica4/testfork.c
@@ -7,5 +7,7 @@ int main() {
     int pid = fork();
     if (pid==0) printf("Soy el hijo\n");
     else printf("Soy el padre\n");
+    fork();
+    fork();
     printf("cuantas veces otra vez\n");
 }
