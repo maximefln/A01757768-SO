ff766c0ffe7c50cd85d8af85a797dd789a1d602d
maximefln <maxime.fillion@hei.yncrea.fr>
2020-03-29 13:19:25 +0000

    practica 4

---
+++ practica4/testfork.c
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+    printf("cuantas veces\n");
+    int pid = fork();
+    if (pid==0) printf("Soy el hijo\n");
+    else printf("Soy el padre\n");
+    printf("cuantas veces otra vez\n");
+}
